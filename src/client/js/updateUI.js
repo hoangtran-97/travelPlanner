f6f4310d89a061135040486c18469107858fcf89
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-01-25 18:52:23 +0700

    feat: change image

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -7,9 +7,11 @@ const updateUI = async () => {
         const destination = document.getElementById("result-destination")
         const departure = document.getElementById("result-departure-date")
         const departureETA = document.getElementById("result-time-to-departure")
+        const result = document.getElementById("result")
         destination.innerHTML = `${name}, ${countryName}`
         departure.innerHTML = `Departure date: ${departureDate}`
         departureETA.innerHTML = `Time to departure: 123`
+        result.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?travel")`
     }
     catch (error) {
         console.log("error", error)
