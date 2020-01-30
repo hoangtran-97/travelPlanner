282cdef04f13cf4857feb83108c0041f1e42c7c6
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-01-30 17:21:31 +0700

    feat: update UI weather

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -3,14 +3,18 @@ const updateUI = async () => {
     try {
         const projectData = await request.json();
         const { departureDate, departureETA, locationInfo, weatherInfo, destinationInfo } = projectData["destinationData"]
+        const { summary, temperature } = weatherInfo.currently
         const { name, countryName } = locationInfo.geonames[0]
         const elementDestination = document.getElementById("result-destination")
         const elementDepartureDate = document.getElementById("result-departure-date")
         const elementDepartureETA = document.getElementById("result-time-to-departure")
+        const elementWeather = document.getElementById("result-weather")
+        const elementWeatherArrival = document.getElementById("result-weather-arrival")
         const result = document.getElementById("result")
         elementDestination.innerHTML = `${name}, ${countryName}`
         elementDepartureDate.innerHTML = `Departure date: ${departureDate}`
         elementDepartureETA.innerHTML = `Time to departure: ${departureETA} days`
+        elementWeather.innerHTML = `Current weather: ${temperature}&#176 C, ${summary}`
         result.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?travel")`
     }
     catch (error) {
