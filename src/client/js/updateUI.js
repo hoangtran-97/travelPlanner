1f015e3836417d875b04ebc32e357e9627ce85b8
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-02-10 10:49:54 +0200

    feat: forecast icon

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -22,6 +22,48 @@ const updateUI = async () => {
         elementWeather.innerHTML = `Current weather: ${temperature}&#176 C, ${summary}`
         elementWeatherArrival.innerHTML = `Expected arrival forecast: ${temperaturePrediction}&#176 C, ${summaryPrediction}`
         result.style.backgroundImage = `url(${webformatURL})`
+
+        switch (icon) {
+            case "clear-day":
+                lottiePlayer.load(lottie.clearDay)
+                break;
+            case "clear-night":
+                lottiePlayer.load(lottie.clearNight)
+                break;
+            case "rain":
+                lottiePlayer.load(lottie.rain)
+                break;
+            case "snow":
+                lottiePlayer.load(lottie.snow)
+                break;
+            case "wind":
+                lottiePlayer.load(lottie.wind)
+                break;
+            case "fog":
+                lottiePlayer.load(lottie.fog)
+                break;
+            case "cloudy":
+                lottiePlayer.load(lottie.cloudy)
+                break;
+            case "partly-cloudy-day":
+                lottiePlayer.load(lottie.partlyCloudyDay)
+                break;
+            case "partly-cloudy-night":
+                lottiePlayer.load(lottie.partlyCloudyNight)
+                break;
+            case "thunderstorm":
+                lottiePlayer.load(lottie.thunderstorm)
+                break;
+            case "hail":
+                lottiePlayer.load(lottie.hail)
+                break;
+            case "tornado":
+                lottiePlayer.load(lottie.tornado)
+                break;
+            default:
+                lottiePlayer.load(lottie.all)
+        }
+        lottiePlayer.setSpeed(1)
     }
     catch (error) {
         console.log("error", error)
