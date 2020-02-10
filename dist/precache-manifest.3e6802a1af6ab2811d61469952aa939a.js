32b2de1dd174fa78bf0d9a885fe5a36d46728a63
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-02-10 13:12:59 +0200

    feat: service worker

---
+++ dist/precache-manifest.3e6802a1af6ab2811d61469952aa939a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b61ab689e33e84582529f76f7360d0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "205f25c1ea9acd76af87",
+    "url": "main.css"
+  },
+  {
+    "revision": "205f25c1ea9acd76af87",
+    "url": "main.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "main.js.LICENSE"
+  }
+]);
