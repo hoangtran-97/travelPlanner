a130d7624823b50f22d8fe0fb01763fc68688cb3
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-09 15:28:45 +0200

    fix

--- src/client/js/api.js
+++ src/client/js/api.js
@@ -1,4 +1,4 @@
-const API_GEONAMES = "https://api.geonames.org/searchJSON?name_startsWith=";
+const API_GEONAMES = "http://api.geonames.org/searchJSON?name_startsWith=";
 const API_DARKSKY = "https://api.darksky.net/forecast/";
 const API_PIXABAY = "https://pixabay.com/api/?key=";
 
